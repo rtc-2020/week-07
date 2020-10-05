733b06ab4866f522ed6c78fa0e94e81123e2be49
Karl Stolley <karl.stolley@gmail.com>
2020-10-05 15:55:31 -0400

    Add ability to see own video

--- site.js
+++ site.js
@@ -2,6 +2,21 @@ if ('mediaDevices' in navigator) {
 
   // Educational array for holding all devices
   var all_devices = [];
+  var global_stream;
+
+  var camera_button = document.querySelector('#start-camera');
+  camera_button.addEventListener('click', function(e) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function(stream) {
+        var camera_video = document.querySelector('#camera');
+        camera_video.srcObject = stream;
+        global_stream = steam;
+      })
+      .catch(function(error) {
+        console.error('Stream error:', error);
+      });
+  });
+
 
   function showDevices() {
     navigator.mediaDevices.enumerateDevices()
